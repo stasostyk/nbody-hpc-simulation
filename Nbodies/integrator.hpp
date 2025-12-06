bff602ad4c6737aa08b02e531911d7b22f16a2e6
Malak Matar <malak@Malaks-Laptop.local>
2025-12-06 14:35:04 +0300

    4th-order Runge–Kutta

--- Nbodies/integrator.hpp
+++ Nbodies/integrator.hpp
@@ -4,3 +4,4 @@
 void euler(std::vector<Body> &bodies, double dt);
 void sympletic_euler(std::vector<Body> &bodies, double dt);
 void velocity_verlet(std::vector<Body> &bodies, double dt);
+void rk4(std::vector<Body> &bodies, double dt);
